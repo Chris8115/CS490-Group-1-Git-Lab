d4d07541953d551240e067562d2ab5c1f62ace6b
ezhang <ericzhang200@gmail.com>
2025-02-13 18:47:56 -0500

    java file that prints out names of group members

---
+++ main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Eric Zhang");
+        System.out.println("Ryan Ramos");
+        System.out.println("Amogh Dalal");
+        System.out.println("Zain Abbas");
+        System.out.println("Chris Defeo");
+    }
+}
